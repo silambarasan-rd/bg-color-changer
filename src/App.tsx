b0be12be45c4988607f25fd8c27eb556fcc7c558
Silambarasan R <silambu1821944@gmail.com>
2023-08-05 20:14:53 +0530

    Color Picker and BG change implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Card, CardBody, Text } from '@chakra-ui/react';
+import Colorful from '@uiw/react-color-colorful';
 import './App.css';
 
 function App() {
+
+  const [hex, setHex] = useState("#065a82");
+
+  useEffect(() => {
+    document.body.style.backgroundColor = hex;
+  }, [hex])
+
   return (
     <div className="container">
       <div className="row">
         <div className="col">
           <div className="app-wrapper">
-            <Card className="flex-grow-1">
+            <Card className="flex-grow-1 flex-md-grow-0">
               <CardBody>
-                <Text fontSize="5xl" fontWeight="black" className="m-0 text-center">BG Color Changer!</Text>
+                <Text fontSize={{ base: '2xl', sm: '3xl', md: '3xl', lg: '4xl' }} fontWeight="black" className="text-center mb-4 mb-md-5 px-md-4 px-1">BG Color Changer!</Text>
+
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                  <Colorful
+                    color={hex}
+                    onChange={(color) => {
+                      setHex(color.hexa);
+                    }}
+                  />
+                  <div style={{ background: hex, marginTop: 30, padding: 10 }}>
+                    {hex}
+                  </div>
+                </div>
+
+                <div className="mt-4 px-2">
+                  <p className="text-center">Developed & Experimented by <br /><strong>Silambarasan R 🤠</strong></p>
+                </div>
               </CardBody>
             </Card>
           </div>
